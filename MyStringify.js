cc9267a2170cb5792138a199b6f36ebd9902e0e6
Kirill <morozoff.gamemode@yandex.ru>
2023-10-31 17:06:13 +0300

    Цикл попытка номер 2

--- MyStringify.js
+++ MyStringify.js
@@ -1,5 +1,4 @@
 
-<<<<<<< HEAD
 function serialize(obj) {
     const visited = new WeakSet();
 
@@ -73,66 +72,6 @@ function serialize(obj) {
         serializeObj += "}";
         return serializeObj;
     }
-=======
-function serSring(obj) {
-    return "\"" + obj + "\"";
-}
-
-function serObject(obj) {
-    if (Array.isArray(obj)) {
-        return serObjectArray(obj);
-    } else if (obj instanceof Date) {
-        return serObjectDate(obj);
-    } else {
-        return serObjectLiteral(obj);
-    }
-}
-
-function serObjectDate(obj) {
-    obj = "\"" + obj.getFullYear() + '-' + ("0"+(obj.getMonth()+1)).slice(-2) + '-' + ("0"+(obj.getDate())).slice(-2) +
-    "T" + ("0" + (obj.getHours()-3)).slice(-2) + ":" + ("0" + obj.getMinutes()).slice(-2) + ":" + 
-    ("0" + obj.getSeconds()).slice(-2) + "." + ("0" + obj.getMilliseconds()).slice(-3) + "Z\"";
-    return obj.toString();
-}
-
-function serObjectArray(obj) {
-    let serializeObj = "[";
-
-    for (let i in obj) {
-        serializeObj += serialize(obj[i]);
-        if (i < obj.length - 1) {
-            serializeObj += ","
-        }
-    }
-
-    serializeObj += "]";
-    return serializeObj;
-}
-
-function serObjectLiteral(obj) {
-    let serializeObj = "{";
-
-    for (const key in obj) {
-        serializeObj += "\"" + key + "\"" + ":" + serialize(obj[key]) + ",";
-    }
-
-    serializeObj = serializeObj.slice(0, -1);
-    serializeObj += "}";
-    return serializeObj;
-}
-
-function serialize(obj) {
-    if (typeof obj !== undefined && obj !== null) {
-        switch (typeof obj) {
-            case "string": 
-                return serSring(obj);
-            case "object":
-                return serObject(obj);
-            default:
-                return String(obj);
-        }
-    }  
->>>>>>> dda474629b3a390e5f973e43834154f721d3fd09
 }
 
 //проверка на цикл
@@ -148,7 +87,6 @@ objects = {
       country: "Россия",
     },
     date: new Date,
-<<<<<<< HEAD
     bool: true,
     circular: null,
 }
@@ -158,10 +96,3 @@ objects.circular = objects;
 for (const key in objects) {
     console.log(serialize(objects[key]) === JSON.stringify(objects[key]) ? true: serialize(objects[key])+" ||| "+JSON.stringify(objects[key]));
 }
-=======
-}
-
-for (const key in objects) {
-    console.log(serialize(objects[key]) === JSON.stringify(objects[key]) ? true: serialize(objects[key])+" ||| "+JSON.stringify(objects[key]));
-}
->>>>>>> dda474629b3a390e5f973e43834154f721d3fd09
